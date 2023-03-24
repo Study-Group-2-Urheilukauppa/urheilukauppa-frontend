c8f6c00c60ef4b666e557f205a88a950ff4f0feb
JoonasMattinen <N2majo02.students.oamk.fi>
2023-03-24 11:53:02 +0200

    add code to winter.js to test database connection

--- src/pages/Winter.js
+++ src/pages/Winter.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const URL = "http://localhost:3000/products/getproducts.php"
 
 export default function Winter() {
+
+    const [category, setCategory] = useState([])
+
+    useEffect(() => {
+
+        const criteria = "1";
+        const address = URL + "/" + criteria;
+
+        axios.get(address)
+            .then((response) => {
+                setCategory(response.data)
+            }).catch(error => {
+                alert(error)
+            })
+    }, [])
+
+
     return (
-<>
-<main className="mb-auto mt-20 bg-white grid content-center justify-center w-600">Talviurheilu
-</main>
-</>
+        <>
+            <main className="mb-auto mt-20 bg-white grid content-center justify-center w-600">Talviurheilu
+            </main>
+            <ol>
+                <li>{category}</li>             
+            </ol>
+        </>
     )
 }
