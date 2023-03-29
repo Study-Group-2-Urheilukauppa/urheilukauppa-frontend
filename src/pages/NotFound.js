6cfbb10ff591a3a4d00619d7cd8b1a53c7f6aba7
matiyp <n2ylma00@students.oamk.fi>
2023-03-29 11:25:58 +0300

    push link to homepage on not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <div className="grid place-items-center h-screen text-3xl">
             404 Page not found.
-           <Link to='/'>Tästä takasin etusivulle</Link>
+           <Link to='/' class=' border-2 bg-secondary'>Tästä takasin etusivulle</Link>
         </div>
     )
 }
