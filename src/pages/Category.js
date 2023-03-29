1b1a3723c86dbdf5327d9ae355fcd97dd0b80444
Rikhardson <n2puri00@students.oamk.fi>
2023-03-29 12:52:08 +0300

    Sale price added to categories and products

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Link, useParams} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import NotFound from './NotFound';
 
 const URL = "http://localhost:3000/products/getproducts.php"
 
 export default function Category() {
 
-    const {categoryId} = useParams();
+    const { categoryId } = useParams();
     const [category, setCategory] = useState("");
     const [products, setProducts] = useState([]);
     const criteria = categoryId;
@@ -23,29 +23,45 @@ export default function Category() {
         }
     }
 
-    useEffect( () => {
+    useEffect(() => {
         getJson()
-        }, [categoryId])
+    }, [categoryId])
 
 
     return (
         <>  {products ?
             <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
                 <div className="text-2xl font-bold">{category}</div><br></br>
-           
-            <div className="grid grid-cols-4 gap-5">
-            
-                 {products.map(x => 
-                        <Link to={`../product/${x.productid}`} key={x.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
-                        <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
-                        <div className="text-xl font-bold">{x.productname}</div> 
-                        <div>{x.descript}</div>
-                        <div className="text-xl font-bold">{x.price} €</div>
-                        </Link>
-                    )}
-            </div>
+
+                <div className="grid grid-cols-4 gap-5">
+
+                    {products.map(x => {
+                        if (x.sale !== null) {
+                            return (
+
+                                <Link to={`../product/${x.productid}`} key={x.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
+                                    <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
+                                    <div className="text-xl font-bold">{x.productname}</div>
+                                    <div>{x.descript}</div>
+                                    <p className="text-xl text-alered font-bold">{(((100 - x.sale) / 100) * x.price).toFixed(2)} € ALE-HINTA!</p>
+                                    <p className="text-xl font-bold line-through">{x.price} €</p>
+                                </Link>
+                            );
+                        } else {
+                            return (
+
+                                <Link to={`../product/${x.productid}`} key={x.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
+                                    <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
+                                    <div className="text-xl font-bold">{x.productname}</div>
+                                    <div>{x.descript}</div>
+                                    <div className="text-xl font-bold">{x.price} €</div>
+                                </Link>
+                            )
+                        }
+                    })}
+                </div>
             </main>
-            : <NotFound /> }
+            : <NotFound />}
         </>
     )
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import NotFound from './NotFound';
 
 const URL = "http://localhost:3000/products/getproduct.php"
 
 export default function Product() {
-    const {productId} = useParams();
+    const { productId } = useParams();
     const [product, setProduct] = useState([]);
 
     useEffect(() => {
@@ -25,28 +24,45 @@ export default function Product() {
 
     return (
         <>
-            {(!product.length == 0) ?
             <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
-           
-            <div>
-            
-                {product.map(x => 
 
-                        <div className="grid grid-cols-2 text-xl font-bold gap-5" key={x.productid}>
-                            <div className="max-w-2xl">
+                <div>
 
-                        <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
-                        </div>
+                    {product.map(x => {
+                        if (x.sale !== null) {
+                            return (
+                                <div className="grid grid-cols-2 text-xl font-bold gap-5" key={x.productid}>
+                                    <div className="max-w-2xl">
 
-                        <div>{x.productname}<br/><br/><p className="font-normal max-w-2xl">{x.fulldescript}</p><br/>
-                        <p className="text-xl font-bold">{x.price} €</p>
-                        </div>
-                        </div>
-                    )}     
-            
-            </div>
+                                        <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
+                                    </div>
+
+                                    <div>{x.productname}<br /><br /><p className="font-normal max-w-2xl">{x.fulldescript}</p><br />
+                                        <p className="text-3xl text-alered font-bold">{(((100 - x.sale) / 100) * x.price).toFixed(2)} € ALE-HINTA!</p>
+                                        <p className="text-3xl font-bold line-through">{x.price} €</p>
+                                    </div>
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div className="grid grid-cols-2 text-xl font-bold gap-5" key={x.productid}>
+                                    <div className="max-w-2xl">
+
+                                        <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
+                                    </div>
+
+                                    <div>{x.productname}<br /><br /><p className="font-normal max-w-2xl">{x.fulldescript}</p><br />
+                                        <p className="text-3xl font-bold">{x.price} €</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+
+
+                    })}
+
+                </div>
             </main>
-            : <NotFound />}
         </>
     )
 }
