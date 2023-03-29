f646be7cdc04b4080908c88f00cd05095b4120df
matiyp <n2ylma00@students.oamk.fi>
2023-03-29 11:21:47 +0300

    Added link to home page on not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
+
 export default function NotFound() {
     return (
         <div className="grid place-items-center h-screen text-3xl">
             404 Page not found.
+           <Link to='/'>Tästä takasin etusivulle</Link>
         </div>
     )
 }
