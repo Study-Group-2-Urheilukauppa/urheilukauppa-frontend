14deb2be09a06e32bba707366bc56a5d3f3e0eaa
oamk-isri <n2isri00@students.oamk.fi>
2023-03-29 11:40:51 +0300

    fix 404 for categories

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -29,23 +29,23 @@ export default function Category() {
 
 
     return (
-        <>  
+        <>  {products ?
             <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
                 <div className="text-2xl font-bold">{category}</div><br></br>
            
             <div className="grid grid-cols-4 gap-5">
             
-                {products ? products.map(x => 
+                 {products.map(x => 
                         <Link to={`../product/${x.productid}`} key={x.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
                         <img src={process.env.PUBLIC_URL + "../" + x.imgURL} alt={x.productname}></img>
                         <div className="text-xl font-bold">{x.productname}</div> 
                         <div>{x.descript}</div>
                         <div className="text-xl font-bold">{x.price} €</div>
                         </Link>
-                    ): <NotFound />} 
-            
+                    )}
             </div>
             </main>
+            : <NotFound /> }
         </>
     )
 }
