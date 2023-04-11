cab18f354393d105b523da6212add71ae5496b1e
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-11 11:52:20 +0300

    added check if user is admin or not

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const URL = "http://localhost:3000/login.php";
@@ -9,7 +9,7 @@ export default function Login() {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -20,9 +20,16 @@ export default function Login() {
 
         axios.post(URL, loginData)
             .then(response => {
+                console.log(response.data)
                 if (response.data.success) {
                     // Redirect the user to a different page
-                    navigate('/DefinitelyNotAdmin');
+
+                    if (response.data.role === 'admin') {
+                        navigate('/DefinitelyNotAdmin')
+                    }
+                    else {
+                        navigate('/')
+                    }
                 } else {
                     // Display an error message
                     alert(response.data.message);
@@ -38,13 +45,13 @@ export default function Login() {
         <div>
             <main className="mx-20 mb-auto mt-40 bg-white grid content-center justify-center w-600">
                 <form className="flex flex-col space-y-2 max-w-sm mx-auto mt-8 p-4 rounded-md shadow-md bg-secondary w-96" onSubmit={handleSubmit}>
-                    <label className="font-semibold text-gray-800"> 
+                    <label className="font-semibold text-gray-800">
                         Käyttäjänimi:
-                        <input className="mt-1 block w-full border rounded-md py-2 px-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" type="text" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+                        <input className="mt-1 block w-full border rounded-md py-2 px-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
                     </label>
                     <label className="font-semibold text-gray-800">
                         Salasana:
-                        <input className="mt-1 block w-full border rounded-md py-2 px-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                        <input className="mt-1 block w-full border rounded-md py-2 px-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                     </label>
                     <button className="bg-secondary hover:bg-third text-white font-bold py-2 px-4 border rounded" type="submit">Kirjaudu sisään</button>
                     <label>Ei tunnuksia?</label>
