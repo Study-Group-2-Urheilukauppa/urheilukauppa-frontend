2bc34477851f12a88bcd6f66449466777c7d3fbc
matiyp <n2ylma00@students.oamk.fi>
2023-04-12 11:04:22 +0300

    little responsive changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,21 +10,22 @@ const URL = "http://localhost:3000/products/getcategories.php"
 
 export default function Navigation() {
 
-  const {categoryId} = useParams();
+  const { categoryId } = useParams();
   const [categories, setCategories] = useState([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 
   useEffect(() => {
 
-      const criteria = categoryId;
-      const address = URL + "/" + criteria;
+    const criteria = categoryId;
+    const address = URL + "/" + criteria;
 
-      axios.get(address)
-          .then((response) => {
-              setCategories(response.data);
-          }).catch(error => {
-              alert(error)
-          })
+    axios.get(address)
+      .then((response) => {
+        setCategories(response.data);
+      }).catch(error => {
+        alert(error)
+      })
   }, [])
 
   return (
@@ -45,11 +46,39 @@ export default function Navigation() {
               </Link>
             </div>
 
-            <div className="text-indigo-600 text-sm font-medium">
-            {categories.map(x => 
-                        <Link className="pl-4 nav-link" to={`/category/${x.categoryid}`} key={x.categoryid}>{x.categoryname}</Link>
-                    )}  
+            <div className="text-indigo-600 text-sm font-medium test">
+              {categories.map(x =>
+                <Link className="pl-4 nav-link" to={`/category/${x.categoryid}`} key={x.categoryid}>{x.categoryname}</Link>
+              )}
             </div>
+
+            <div className="hamburger">
+              <button
+                className="text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900"
+                aria-label="Toggle menu"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                <svg viewBox="0 0 24 24" className="h-12 w-12 pt-1 fill-current">
+                  <rect x="4" y="5" width="16" height="2" />
+                  <rect x="4" y="11" width="16" height="2" />
+                  <rect x="4" y="17" width="16" height="2" />
+                </svg>
+              </button>
+              {isMenuOpen && (
+                <div className="smooth absolute top-16 p-5 left-0 w-screen bg-fifth ">
+                  {categories.map((x) => (
+                    <Link
+                      className="block py-2 px-4 hover:bg-gray-100"
+                      to={`/category/${x.categoryid}`}
+                      key={x.categoryid}
+                    >
+                      {x.categoryname}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+
             <Search />
             <ShoppingCart />
             <LoginButton />
