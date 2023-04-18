6db60c57a7fd77c017f9e793c7132196e0a2e138
Rikhardson <n2puri00@students.oamk.fi>
2023-04-18 13:40:48 +0300

    responsivity

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -38,7 +38,7 @@ export default function Category() {
                 <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
                     <div className="text-md font-bold md:text-lg lg:text-2xl">{category}</div><br></br>
 
-                    <div className="grid grid-cols-4 gap-5">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
 
                         {products.map((result) => (
                             <Link to={`../product/${result.productid}`} key={result.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
