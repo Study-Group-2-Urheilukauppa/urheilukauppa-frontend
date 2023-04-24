88d5b4cbd0a84e4a74250e8becb3e5c3549516c5
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-24 12:07:47 +0300

    added comments to code

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
+// URL address variable
 const URL = 'http://localhost:3000/viewOrders.php';
 
 function Orders() {
     const [order, setOrder] = useState([]);
 
+    //get data from backend
     useEffect(() => {
         axios.get(URL)
             .then((response) => {
+                // store data to order useState
                 setOrder(response.data);
             })
             .catch(error => {
@@ -16,10 +19,11 @@ function Orders() {
             });
     }, []);
 
+    // map results
     return (
-        <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
+        <main className="mx-20 mb-auto mt-20 bg-white grid justify-left w-600">
             <div>
-                <h1 className="text-2xl font-bold mb-4">Tilaukset</h1>
+                <h1 className="text-2xl font-bold mb-4">Tilaukset</h1>              
                 {order.map((item) => (
                     <div key={item.ordernum} className="border p-4 mb-4">
                         <p className="text-lg font-medium">AsiakasID: {item.clientid}</p>
