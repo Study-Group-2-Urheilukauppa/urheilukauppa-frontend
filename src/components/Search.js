c156b05df6d3146ec889066b0ad6007d187b0a76
Rikhardson <n2puri00@students.oamk.fi>
2023-04-24 10:40:29 +0300

    Enter-press added to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,7 +20,7 @@ export default function Search() {
   }, [searchTerm]);
 
   const handleSearch = () => {
-    navigate('../SearchResult', { state: { results: results }});
+    navigate('../SearchResult', { state: { results: results } });
   };
 
   return (
@@ -32,6 +32,11 @@ export default function Search() {
               type="text"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSearch();
+                }
+              }}
               className="relative m-0 block w-[1%] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-indigo-600 text-sm font-medium text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
               placeholder="Hae tuotetta"
               aria-label="Search"
