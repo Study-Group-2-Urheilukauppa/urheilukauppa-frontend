1e4149800dc2a8427920db8799f04b1d82447cd4
JoonasMattinen <N2majo02.students.oamk.fi>
2023-04-25 14:48:02 +0300

    changed styles

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -20,7 +20,7 @@ export default function SearchResult() {
 
   return (
     <main className="mx-20 mb-auto mt-20 bg-white grid content-center justify-center w-600">
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
 
         {results.map((result) => (
           <Link to={`../product/${result.productid}`} key={result.productid} className="hover:bg-secondary hover:bg-opacity-20 p-2">
